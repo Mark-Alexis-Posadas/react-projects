1742a5de793c04c3102683331ae19b30bf624f60
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-17 13:54:55 +0800

    added accordion file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 const App = () => {
-  return (
-    <div>
-      <button onClick={handleClick}>1</button>
-      <button onClick={handleClick}>2</button>
-      <button onClick={handleClick}>3</button>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
