65d56bf72a3047704a45d0f42c0e4ec409def6f5
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-18 07:39:38 +0800

    done installing font awesome

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 import accordionItems from "./data";
 export default function Accordion() {
+  const [activeAccordion, setActiveAccordion] = useState(accordionItems[0].id);
+
+  const toggleAccordion = (id) => {
+    setActiveAccordion(id === activeAccordion ? null : id);
+  };
+
   return (
     <>
+      <h2>Project 2: FAQ/Accordion</h2>
       {accordionItems.map((accordion) => (
         <AccordionItem
           title={accordion.title}
           description={accordion.content}
           key={accordion.id}
+          accordionId={accordion.id}
+          toggleAccordion={toggleAccordion}
         />
       ))}
     </>
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,13 +1,25 @@
-export default function AccordionItem({ title, description }) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+export default function AccordionItem({
+  title,
+  description,
+  toggleAccordion,
+  accordionId,
+}) {
   return (
-    <ul>
-      <li>
+    <ul className="my-3">
+      <li
+        className="flex items-center justify-between cursor-pointer bg-slate-50 shadow-md p-2 rounded"
+        onClick={() => toggleAccordion(accordionId)}
+      >
         {title}
-        <span></span>
+        <span>
+          <FontAwesomeIcon icon={faPlus} />
+        </span>
       </li>
-      <ul>
-        <li>{description}</li>
-      </ul>
+      <div>
+        <p>{description}</p>
+      </div>
     </ul>
   );
 }
