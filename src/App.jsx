70a4c2741f998bbcad373fbd34344f441d5cb197
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-17 13:53:03 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <button onClick={handleClick}>1</button>
+      <button onClick={handleClick}>2</button>
+      <button onClick={handleClick}>3</button>
+    </div>
+  );
+};
+
+export default App;
