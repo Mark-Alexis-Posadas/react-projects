4ea8e96319b65c5a8f012788f589d96f1a776d91
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-18 07:47:59 +0800

    done accordion functionality

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 import accordionItems from "./data";
+
 export default function Accordion() {
   const [activeAccordion, setActiveAccordion] = useState(accordionItems[0].id);
 
@@ -11,13 +12,15 @@ export default function Accordion() {
   return (
     <>
       <h2>Project 2: FAQ/Accordion</h2>
+
       {accordionItems.map((accordion) => (
         <AccordionItem
           title={accordion.title}
           description={accordion.content}
           key={accordion.id}
-          accordionId={accordion.id}
+          id={accordion.id}
           toggleAccordion={toggleAccordion}
+          isActive={accordion.id === activeAccordion}
         />
       ))}
     </>
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,25 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
 export default function AccordionItem({
   title,
   description,
+  id,
   toggleAccordion,
-  accordionId,
+  isActive,
 }) {
   return (
     <ul className="my-3">
       <li
         className="flex items-center justify-between cursor-pointer bg-slate-50 shadow-md p-2 rounded"
-        onClick={() => toggleAccordion(accordionId)}
+        onClick={() => toggleAccordion(id)}
       >
         {title}
         <span>
-          <FontAwesomeIcon icon={faPlus} />
+          <FontAwesomeIcon icon={isActive ? faMinus : faPlus} />
         </span>
       </li>
-      <div>
-        <p>{description}</p>
-      </div>
+      {isActive && (
+        <div>
+          <p>{description}</p>
+        </div>
+      )}
     </ul>
   );
 }
