f0a708d84f01fc80a032d42970cef0b81512d0a9
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-18 14:18:45 +0800

    optimize tab color

--- src/components/Tab/TabItem.jsx
+++ src/components/Tab/TabItem.jsx
@@ -4,9 +4,15 @@ export default function TabItem({ tabItems, handleChangeTab, activeTab }) {
       <ul className="flex items-center">
         {tabItems.map((item, idx) => (
           <li
-            className={`w-[9rem] h-16 mx-2 shadow-md flex items-center justify-center cursor-pointer ${
+            className={`w-[9rem] h-16  flex items-center justify-center cursor-pointer ${
               activeTab === idx
-                ? "bg-purple-600 text-white"
+                ? idx === 0
+                  ? "bg-green-400 text-white"
+                  : idx === 1
+                  ? "bg-purple-400 text-white"
+                  : idx === 2
+                  ? "bg-red-400 text-white"
+                  : "bg-orange-400 text-white"
                 : "bg-slate-50 text-black"
             }`}
             key={idx}
@@ -20,9 +26,17 @@ export default function TabItem({ tabItems, handleChangeTab, activeTab }) {
       {tabItems.map((item, idx) => (
         <div
           key={idx}
-          className={`${
-            activeTab === idx ? "block" : "hidden"
-          } p-10 max-w-[900px]`}
+          className={`${activeTab === idx ? "block" : "hidden"} ${
+            idx === 0
+              ? "bg-green-400"
+              : idx === 1
+              ? "bg-purple-400"
+              : idx === 2
+              ? "bg-red-400"
+              : idx === 3
+              ? "bg-orange-400"
+              : ""
+          } p-10 max-w-[900px] text-white`}
         >
           {item.content}
         </div>
