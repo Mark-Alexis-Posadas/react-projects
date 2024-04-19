c7b2f2fe9ad99fbcd6f03279b876c82b8652b3e6
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-19 08:03:23 +0800

    done theme

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,27 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThemeItem from "./ThemeItem";
 import themeData from "./data";
 
 export default function ThemeSelector() {
   const [selectedTheme, setSelectedTheme] = useState(() =>
-    localStorage.getItem("theme")
+    localStorage.getItem("selectedTheme")
   );
 
   const handleClick = (themeColor) => {
     setSelectedTheme(themeColor);
   };
 
+  useEffect(() => {
+    localStorage.setItem("selectedTheme", selectedTheme);
+  }, [selectedTheme]);
+
   return (
     <>
-      <div className={`w-full h-[350px] ${selectedTheme}`}></div>
-      {themeData.map((theme, index) => (
-        <ThemeItem
-          key={index}
-          currentTheme={selectedTheme}
-          theme={theme}
-          handleClick={handleClick}
-        />
-      ))}
+      <div className={`w-full h-[750px] ${selectedTheme}`}></div>
+      <div className="flex items-center shadow-md bg-slate-50 p-3">
+        {themeData.map((theme, index) => (
+          <ThemeItem
+            key={index}
+            currentTheme={selectedTheme}
+            theme={theme}
+            handleClick={handleClick}
+          />
+        ))}
+      </div>
     </>
   );
 }
